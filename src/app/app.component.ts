af22ec44f2dd9635f9da17a5f0e0cee59c5d3eb5
Denys Vuika <denys.vuika@gmail.com>
2023-05-07 10:44:43 +0100

    cleanup app code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { NgBpmnComponent } from '@denysvuika/ng-bpmn';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, NgBpmnComponent],
+  imports: [RouterModule, NgBpmnComponent],
   selector: 'ng-bpmn-app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
