b829e99155229d3355738d241ebd65a4c7076bcb
Denys Vuika <denys.vuika@gmail.com>
2023-05-07 10:22:56 +0100

    simple component implementation

--- libs/ng-bpmn/src/lib/ng-bpmn/ng-bpmn.component.ts
+++ libs/ng-bpmn/src/lib/ng-bpmn/ng-bpmn.component.ts
@@ -1,12 +1,97 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import {
+  AfterContentInit,
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  Output,
+  SimpleChanges,
+  ViewChild,
+  ViewEncapsulation,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import BpmnJS from 'bpmn-js/lib/Modeler';
+import { Observable, Subscription, from, map, switchMap } from 'rxjs';
 
 @Component({
   selector: 'ng-bpmn',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './ng-bpmn.component.html',
   styleUrls: ['./ng-bpmn.component.scss'],
   encapsulation: ViewEncapsulation.None,
 })
-export class NgBpmnComponent {}
+export class NgBpmnComponent
+  implements OnInit, AfterContentInit, OnChanges, OnDestroy
+{
+  private bpmnJS: any = new BpmnJS();
+
+  @Input() url?: string;
+
+  @ViewChild('ref', { static: true })
+  private el?: ElementRef;
+
+  @Output()
+  importDone: EventEmitter<any> = new EventEmitter();
+
+  constructor(private http: HttpClient) {
+    this.bpmnJS.on('import.done', ({ error }: any) => {
+      if (!error) {
+        this.bpmnJS.get('canvas').zoom('fit-viewport');
+      }
+    });
+  }
+
+  ngAfterContentInit(): void {
+    this.bpmnJS.attachTo(this.el?.nativeElement);
+  }
+
+  ngOnInit(): void {
+    if (this.url) {
+      this.loadUrl(this.url);
+    }
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['url']) {
+      this.loadUrl(changes['url'].currentValue);
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.bpmnJS.destroy();
+  }
+
+  loadUrl(url: string): Subscription {
+    return this.http
+      .get(url, { responseType: 'text' })
+      .pipe(
+        switchMap((xml: string) => this.importDiagram(xml)),
+        map((result) => result.warnings)
+      )
+      .subscribe(
+        (warnings) => {
+          this.importDone.emit({
+            type: 'success',
+            warnings,
+          });
+        },
+        (err) => {
+          this.importDone.emit({
+            type: 'error',
+            error: err,
+          });
+        }
+      );
+  }
+
+  private importDiagram(xml: string): Observable<{ warnings: Array<any> }> {
+    return from(
+      this.bpmnJS.importXML(xml) as Promise<{ warnings: Array<any> }>
+    );
+  }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,4 +12,21 @@ import { NgBpmnComponent } from '@ngstack/ng-bpmn';
 })
 export class AppComponent {
   title = 'ng-bpmn-app';
+  importError?: Error;
+  diagramUrl =
+    'https://cdn.staticaly.com/gh/bpmn-io/bpmn-js-examples/dfceecba/starter/diagram.bpmn';
+
+  handleImported(event: any) {
+    const { type, error, warnings } = event;
+
+    if (type === 'success') {
+      console.log(`Rendered diagram (%s warnings)`, warnings.length);
+    }
+
+    if (type === 'error') {
+      console.error('Failed to render diagram', error);
+    }
+
+    this.importError = error;
+  }
 }
